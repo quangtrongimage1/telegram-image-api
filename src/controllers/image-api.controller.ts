ac3a3b8eb594b1c9efddaf153feb89bcb95b5134
quangtrong1506 <quangtrong1506@gmail.com>
2026-09-19 05:04:31 +0700

    add type globalThis.Response

--- src/controllers/image-api.controller.ts
+++ src/controllers/image-api.controller.ts
@@ -123,9 +123,9 @@ export class ImageApiController {
             if (!downloadUrl) throw new Error('Failed to get file URL');
             urlImageCache.set(fileId, downloadUrl);
          }
-         const response = await fetch(downloadUrl);
+         const response: globalThis.Response = await fetch(downloadUrl);
 
-         if (!response.ok || !response.body) {
+         if (response.ok || !response.body) {
             return res.status(response.status).send('Image not found');
          }
 
